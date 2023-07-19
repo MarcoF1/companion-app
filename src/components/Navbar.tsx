8e78725e9f562aa507412b4cdccc09ec9e3054b7
MarcoF1 <marco1fleming@gmail.com>
2023-07-18 21:20:47 -0400

    fixed build error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,7 +25,6 @@ export default function Navbar() {
                 {navigation.map((item) => (
                   <a
                     key={item.name}
-                    href={item.href}
                     className={classNames(
                       item.current
                         ? "bg-gray-900 text-white"
